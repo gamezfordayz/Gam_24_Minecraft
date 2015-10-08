2f7b790523c252d2f163141bdf84621f06558520
ocknon <ockn0n@hotmail.com>
2015-10-07 18:55:09 -0700

    Complete Crafting

--- Minecraft_Project/Assets/Scripts/InventoryManager.cs
+++ Minecraft_Project/Assets/Scripts/InventoryManager.cs
@@ -12,15 +12,92 @@ public class InventoryManager : MonoBehaviour {
 
 	public AudioClip pickUpSound;
 
+	Crafting crafting;
+
+	public GameObject craftingButton;
+	public GameObject inventoryCraft;
+
 	public bool inventoryIsFull = false;
+	public bool masterCrafter = false;
 	//public GameObject mainInventory;
 	//public GameObject activeInventory;
 	// Use this for initialization
 	void Awake () 
 	{
 		inventoryManager = this;
+		crafting = GetComponent<Crafting> ();
 	}
-	
+
+	void Update()
+	{
+		if (!inventoryCraft.active)
+			CallCrafting (craftingTableInventorySlots, outputInventorySlots);
+		else
+			CallCrafting (craftingInventorySlots, outputInventorySlots);
+
+		if (masterCrafter)
+			craftingButton.SetActive (true);
+		else
+			craftingButton.SetActive (false);
+	}
+
+	public void CallCrafting(SlotProperties[] craftingInventorySlots, SlotProperties outputInventorySlots)
+	{
+		//pass in input
+		string inputString = null;
+		int output = 0;
+		int numberOfOutput = 1;
+
+		for(int i=0; i<craftingInventorySlots.Length; i++)
+		{
+			inputString += ((int)craftingInventorySlots[i].itemID).ToString() + " ";
+			if (craftingInventorySlots.Length < 9)
+			{
+				if (i == 1)
+					inputString += "0 ";
+				if (i == 3)
+					inputString += "0 0 0 0 ";
+			}
+		}
+
+		if (!masterCrafter)
+		{
+			output = crafting.GetOutput (inputString);
+			numberOfOutput = crafting.GetOutputNumber (inputString);
+
+			outputInventorySlots.itemID = (CubeProperties.itemIDs)output;
+			outputInventorySlots.numberOfItem = (byte)numberOfOutput;
+		}
+	}
+
+	public void SaveXML()
+	{
+		if (!inventoryCraft.active)
+			SaveXMLButton (craftingTableInventorySlots, outputInventorySlots);
+		else
+			SaveXMLButton (craftingInventorySlots, outputInventorySlots);
+	}
+
+	void SaveXMLButton (SlotProperties[] craftingInventorySlots, SlotProperties outputInventorySlots)
+	{
+		string craftingIDs = null;
+		for(int i=0; i<craftingInventorySlots.Length; i++)
+		{
+			craftingIDs += ((int)craftingInventorySlots[i].itemID).ToString() + " ";
+
+			if (craftingInventorySlots.Length < 9)
+			{
+				if (i == 1)
+					craftingIDs += "0 ";
+				if (i == 3)
+					craftingIDs += "0 0 0 0 ";
+			}
+		}
+		craftingIDs += "," + ((int)outputInventorySlots.itemID).ToString() + ",";
+		craftingIDs += outputInventorySlots.numberOfItem + "," + "\n";
+
+		crafting.SaveXML (craftingIDs);
+	}
 
 //	void SortInventorySlots()
 //	{
